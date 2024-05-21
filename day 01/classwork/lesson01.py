5a23b6bd9b6bf233bb64dd48826058ce8ef079a3
DavidNizh24 <danizdaniz1996@gmail.com>
2024-05-22 00:38:02 +0400

    commit

---
+++ day 01/classwork/lesson01.py
@@ -0,0 +1,24 @@
+#name = "davit"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "dato" არის ცვლადისთვის მნიშვნელობა
+
+surname = "nizharadze"
+
+#print(name)
+#print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "davit" #ეს არის str (string) ტიპის ცვლადი
+age = 16 # ეს არის int (integer) მთელი რიცხვი
+height = 181 #ეს არის float ტიპის ცვლადი ანუ (ათწილადი)
+#boolean (bool)
+
+knows_programming = True #true or false
+is_ugly = False #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False # ჯავასკრიპტული camelcase
+
+#print(name + " " + surname)
+
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
---
+++ day 03/homework/homework05.py
@@ -0,0 +1,4 @@
+number_1 = 1
+#snakecase _
+number_2 = 2
+print(number_1 + number_2)
