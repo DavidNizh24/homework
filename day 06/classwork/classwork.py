a796cd9408e4c923f3089f38ab785aa446b1c742
DavidNizh24 <danizdaniz1996@gmail.com>
2024-05-29 21:04:58 +0400

    commit

---
+++ day 06/classwork/classwork.py
@@ -0,0 +1,21 @@
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Please enter second number: "))
+num3 = int(input("Please enter third number: "))
+num4 = int(input("Please enter forth number: "))
+
+print(num1 + num2 + num3 + num4)
+print(num1 - num2 - num3 - num4)
+print(num1 * num2 * num3 * num4)
+print(num1 / num2 / num3 / num4)
+
+
+
+
+
+user_firstname = input("Please enter your firstname: ")
+user_lastname = input("Please enter your lastname: ")
+user_age = input("Please enter your age: ")
+user_email = input("Please enter your email: ")
+
+
+print("Your inputed information is:",user_firstname,user_lastname, user_age, user_email)
